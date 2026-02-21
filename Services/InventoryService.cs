13288b71b914c64954bd96660fac51647e672319
RalphDante <106255342+RalphDante@users.noreply.github.com>
2026-02-21 10:58:51 +0800

    Add InventoryService class for product management
    Implemented InventoryService class to manage product inventory with methods for retrieving product names, stock counts, and updating stock.

--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -1,1 +1,52 @@
+namespace InventoryApp.Services
+{
+    public class InventoryService
+    {
+        private string[,] products;
+        private string[] initialStock;
 
+        public InventoryService()
+        {
+            products = new string[2, 3]
+            {
+                { "Apples", "Milk", "Bread" },
+                { "10", "5", "20" }
+            };
+
+            initialStock = new string[] { "10", "5", "20" };
+        }
+
+        public string[,] GetInventory()
+        {
+            return products;
+        }
+
+        public int GetProductCount()
+        {
+            return products.GetLength(1);
+        }
+
+        public string GetProductName(int index)
+        {
+            return products[0, index];
+        }
+
+        public string GetStock(int index)
+        {
+            return products[1, index];
+        }
+
+        public void UpdateStock(int index, string newStock)
+        {
+            products[1, index] = newStock;
+        }
+
+        public void ResetInventory()
+        {
+            for (int i = 0; i < initialStock.Length; i++)
+            {
+                products[1, i] = initialStock[i];
+            }
+        }
+    }
+}
